852021ae617887cf2c83ff37325f3ec910ea649a
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-08-20 19:48:58 +0300

    hero section almost finished

--- cartify_main/src/pages/Home.jsx
+++ cartify_main/src/pages/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import Header from '../components/Header'
+import GlowingBg from '../components/GlowingBg'
+import AnimeCircle from '../components/AnimeCircle'
 
 function Home() {
   return (
-    <div className=''>
-      <h1>Welcome to the Home Page</h1>
-    </div>
+    <section className="bg-black overflow-x-hidden">
+      <Header />
+
+      {/* hero section */}
+      <AnimeCircle />
+
+      <div className='w-full h-16 bg-white mt-4'>
+
+      </div>
+
+      {/* About section */}
+      <section className='h-screen'>
+
+      </section>
+
+
+   
+    </section>
   )
 }
 
